74ddf945ad1727036297ec16a9af1bda2c1ba1b7
Petr <petrkgm@gmail.com>
2016-11-12 00:31:43 +0300

    Clean up

--- src/answers/TopPhrases.java
+++ src/answers/TopPhrases.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package answers;
 
 import java.io.BufferedReader;
@@ -32,7 +27,7 @@ import java.util.stream.Collectors;
 
 /**
  *
- * @author toor
+ * @author petrk
  */
 public class TopPhrases {
     
--- src/answers/TopPhrasesTestGenerator.java
+++ src/answers/TopPhrasesTestGenerator.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 
 /**
  *
- * @author toor
+ * @author petrk
  */
 public class TopPhrasesTestGenerator {
     
