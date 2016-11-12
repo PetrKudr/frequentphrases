aac9665dba1ffc9881b8ee9c6702250a509b69a3
Petr Kudriavtsev <petrkgm@gmail.com>
2016-11-12 04:29:53 +0300

    Reimplemented complimentary pairs

--- src/answers/ComplimentaryPairs.java
+++ src/answers/ComplimentaryPairs.java
@@ -3,7 +3,9 @@ package answers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -26,73 +28,35 @@ public class ComplimentaryPairs {
     }
     
     public static void detect(int array[], int K, Consumer consumer) {
-        if (array.length > 0) {
-            // array with original positions. Stored index doesn't affect on 
-            // order after sorting because it is stored in lower bits
-            long workArray[] = new long[array.length];
-            for (int i = 0; i < array.length; ++i) {
-                workArray[i] = wrapIntInt(array[i], i);
-            }
-            Arrays.sort(workArray);
-            int left = 0;
-            int right = workArray.length - 1;
-            int lVal = firstInt(workArray[left]);
-            int rVal = firstInt(workArray[right]);
-            while (left <= right) {
-                assert lVal <= rVal;
-                int sum = lVal + rVal;
-                if (sum < K) {
-                    lVal = firstInt(workArray[++left]);
-                } else if (sum > K) {
-                    rVal = firstInt(workArray[--right]);
-                } else if (sum == K) {
-                    if (lVal != rVal) {
-                        int rr = right;
-                        while (rVal == workArray[--right]);
-                        rVal = firstInt(workArray[right]);
-                        // (right, rr] - subarray with numbers equal to rVal
-                        
-                        int ll = left;
-                        while (lVal == workArray[++left]);
-                        lVal = firstInt(workArray[left]);
-                        // [ll, left) - subarray with numbers equal to lVal
-                        
-                        for (int l = ll; l < left; ++l) {
-                            for (int r = rr; r > right; --r)  {
-                                consumer.consume(secondInt(workArray[l]), secondInt(workArray[r]));
-                            }
-                        }
-                    } else {
-                        for (int l = left; l <= right; ++l) {
-                            for (int r = right; r >= left; --r)  {
-                                consumer.consume(secondInt(workArray[l]), secondInt(workArray[r]));
-                            }
-                        }
-                        break;
+        Map<Integer, List<Integer>> mapping = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < array.length; ++i) {
+            putInMapOfLists(mapping, array[i], i);
+        }
+        for (int i = 0; i < array.length; ++i) {
+            int complimentary = K - array[i];
+            List<Integer> indexes = mapping.get(complimentary);
+            if (indexes != null) {
+                for (Integer j : indexes) {
+                    // consider (i, j) and (j, i) similar
+                    if (j >= i) {
+                        consumer.consume(i, j);
                     }
                 }
             }
         }
     }
     
+    private static <K, T> void putInMapOfLists(Map<K, List<T>> map, K key, T value) {
+        List<T> list = map.get(key);
+        if (list == null) {
+            list = new ArrayList<>();
+            map.put(key, list);
+        }
+        list.add(value);
+    }
+    
     @FunctionalInterface
     public static interface Consumer {
         void consume(int i, int j);
-    }
-    
-    private static long wrapIntInt(int First, int Second) {
-        long Out = (((long) (First)) << 32) | Integer.toUnsignedLong(Second);
-        assert firstInt(Out) == First;
-        assert secondInt(Out) == Second;
-        return Out;
-    }
-    
-    private static int firstInt(long wrapped) {
-      return (int)(wrapped >>> 32);
-    }
-
-    private static int secondInt(long wrapped) {
-      assert (int)(wrapped & 0xFFFFFFFFL) == (int)wrapped;
-      return (int)(wrapped);
     }
 }
